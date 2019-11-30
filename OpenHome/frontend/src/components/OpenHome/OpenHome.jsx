b10611ab03e28ea3035739e36f45a9b95f05ad7e
prachichouksey <prachi.chouksey@sjsu.edu>
2019-11-29 18:51:02 -0800

    host property changes

--- OpenHome/frontend/src/components/OpenHome/OpenHome.jsx
+++ OpenHome/frontend/src/components/OpenHome/OpenHome.jsx
@@ -1,40 +1,45 @@
-import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import AuthenticatedRoute from './AuthenticatedRoute.jsx'
-import LoginComponent from './LoginComponent.jsx'
-import ErrorComponent from './ErrorComponent.jsx'
-import HeaderComponent from './HeaderComponent.jsx'
-import FooterComponent from './FooterComponent.jsx'
-import LogoutComponent from './LogoutComponent.jsx'
-import WelcomeComponent from './WelcomeComponent.jsx'
-import FrontPage from './FrontPage.jsx'
-import AssetDisplay from './AssetDisplay.jsx'
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AuthenticatedRoute from "./AuthenticatedRoute.jsx";
+import LoginComponent from "./LoginComponent.jsx";
+import ErrorComponent from "./ErrorComponent.jsx";
+import HeaderComponent from "./HeaderComponent.jsx";
+import FooterComponent from "./FooterComponent.jsx";
+import LogoutComponent from "./LogoutComponent.jsx";
+import WelcomeComponent from "./WelcomeComponent.jsx";
+import FrontPage from "./FrontPage.jsx";
+import PropertiesDashboard from "../OwnerProperty/PropertiesDashboard.jsx";
+import PropertyDetails from "../OwnerProperty/PropertyDetails.jsx";
+import CreateProperty from "../OwnerProperty/CreateProperty.jsx";
 
 class OpenHome extends Component {
-    render() {
-        return (
-            <div className="OpenHomeApp">
-                <Router>
-                    <>
-                        <HeaderComponent/>
-                        <Switch>
-                            <Route path="/" exact component={FrontPage}/>
-                            <Route path="/login" component={LoginComponent}/>
-                            <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent}/>
-                          
-                            <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
-                            <Route path="/asset" component={AssetDisplay} />
-                            
-                            <Route component={ErrorComponent}/>
-                        </Switch>
-                        <FooterComponent/>
-                    </>
-                </Router>
-               
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="OpenHomeApp">
+        <Router>
+          <>
+            <HeaderComponent />
+            <Switch>
+              <Route path="/" exact component={FrontPage} />
+              <Route path="/login" component={LoginComponent} />
+              <AuthenticatedRoute
+                path="/welcome/:name"
+                component={WelcomeComponent}
+              />
+
+              <AuthenticatedRoute path="/logout" component={LogoutComponent} />
+              <Route path="/dashboard" component={PropertiesDashboard} />
+              <Route path="/property/new" component={CreateProperty} />
+              <Route path="/property/:propertyId" component={PropertyDetails} />
+
+              <Route component={ErrorComponent} />
+            </Switch>
+            <FooterComponent class="footer" />
+          </>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default OpenHome
+export default OpenHome;
---
+++ OpenHome/frontend/src/components/OwnerProperty/PropertiesDashboard.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { API_URL } from "../../Constants";
+import axios from "axios";
+import Draggable from "react-draggable";
+class PropertiesDashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      properties: []
+    };
+  }
+
+  componentDidMount() {
+    let id = 5;
+    axios
+      .get(API_URL + `/property/owner/${id}`, {
+        headers: { "Content-Type": "application/json" }
+      })
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          properties: this.state.properties.concat(response.data)
+        });
+      });
+  }
+  OpenProperty = property => {
+    this.props.history.push(`/property/${property.propertyId}`);
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <div class="body-div">
+          <br />
+          <h2>Openhome Dashboard</h2>
+          <br />
+          <h4>Welcome Host</h4>
+          <br></br>
+          <Link to="/property/new">
+            <a href="#" class="btn btn-info btn-lg">
+              <span class="glyphicon glyphicon-plus">+</span> Property
+            </a>
+          </Link>
+          <br></br>
+          <br></br>
+          <div class="card-columns">
+            {this.state.properties.map(property => {
+              return (
+                <Draggable>
+                  <div>
+                    <div class="card text-white bg-dark mb-3">
+                      <div class="card-header">{property.name}</div>
+                      <div class="card-body">
+                        <p class="card-text">
+                          <img
+                            src={property.picture}
+                            height="250"
+                            width="300"
+                          />
+                          <table>
+                            <tr>
+                              <th>Property:</th>
+                              <td>{property.propertyDescription}</td>
+                            </tr>
+                          </table>
+                        </p>
+                      </div>
+                      <div class="card-footer">
+                        <button
+                          onClick={() => this.OpenProperty(property)}
+                          class="btn btn-primary"
+                        >
+                          Open Property
+                        </button>
+                        <br /> <br />
+                      </div>
+                    </div>
+                  </div>
+                </Draggable>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PropertiesDashboard;
