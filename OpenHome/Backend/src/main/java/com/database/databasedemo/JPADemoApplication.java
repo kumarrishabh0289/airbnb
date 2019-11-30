dac929bedac20fb3ab99f52d69fa282f744cdba5
prachichouksey <prachi.chouksey@sjsu.edu>
2019-11-29 18:56:13 -0800

    correcting application

--- OpenHome/Backend/src/main/java/com/database/databasedemo/JPADemoApplication.java
+++ OpenHome/Backend/src/main/java/com/database/databasedemo/JPADemoApplication.java
@@ -3,9 +3,6 @@ import com.database.databasedemo.entity.Person;
 import com.database.databasedemo.entity.Property;
 import com.database.databasedemo.repository.PersonSpringDataRepo;
 import com.database.databasedemo.service.PropertyService;
-import com.database.databasedemo.entity.Asset;
-import com.database.databasedemo.repository.AssetSpringDataRepo;
-import com.database.databasedemo.repository.PersonSpringDataRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +19,7 @@ public class JPADemoApplication implements CommandLineRunner {
     PersonSpringDataRepo repo;
 
 
-    @Autowired
-    AssetSpringDataRepo assetRepo;
+
     //PersonJPARepo repo;
 
     @Autowired
@@ -64,13 +60,6 @@ public class JPADemoApplication implements CommandLineRunner {
 
         logger.info("Find By ID -> {}", repo.findById(2));
         repo.deleteById(2);
-
-
-
-        assetRepo.save(new Asset("33 S 3rd Street", "San Jose", "http://media.graytvinc.com/images/wcjb_apartment-living-room.jpg", 3));
-        assetRepo.save(new Asset("101 San Fernando", "San Jose", "http://media.graytvinc.com/images/wcjb_apartment-living-room.jpg", 3));
-        logger.info("Find All -> {}", assetRepo.findByOwner(3));
-
 
 
     }
--- OpenHome/Backend/src/main/java/com/database/databasedemo/entity/Room.java
+++ OpenHome/Backend/src/main/java/com/database/databasedemo/entity/Room.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.util.List;
 
-//@Entity
+@Entity
 @Table(name = "room")
 public class Room {
 
