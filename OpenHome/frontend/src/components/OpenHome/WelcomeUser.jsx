e0c30f24b17b9b4ef03c5c9c59456fb842fff3cc
Kumar Rishabh <kumarrishabh@sjsu.edu>
2019-12-03 03:58:05 -0800

    Added new functionality

--- OpenHome/frontend/src/components/OpenHome/WelcomeUser.jsx
+++ OpenHome/frontend/src/components/OpenHome/WelcomeUser.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../Constants";
 import axios from "axios";
-import Draggable from "react-draggable";
 class WelcomeUser extends Component {
   constructor(props) {
     super(props);
@@ -25,85 +24,73 @@ class WelcomeUser extends Component {
         });
       });
   }
-  OpenProperty = booking => {
-    this.props.history.push(`/booking/${booking.bookingId}`);
+
+  checkIn = (booking) => {
+    alert("Check-in for this Booking ID : "+booking.id)
+  };
+
+  checkOut = (booking) => {
+    alert("Check-Out for this Booking ID : "+booking.id)
+  };
+
+  cancel = (booking) => {
+    alert("Cancel for this Booking ID : "+booking.id)
   };
 
   render() {
     return (
-      <div class="container">
-        <div class="body-div">
-          <br />
-          <h2>Openhome User Dashboard</h2>
-          <br />
-          <h4>Welcome {this.props.match.params.name}</h4>
-          <br></br>
-          <Link to="/property/new">
-            <a href="/#" class="btn btn-info btn-lg">
-              <span class="glyphicon glyphicon-plus">+</span> Property
-            </a>
-          </Link>
-          <br></br>
-          <br></br>
-          <div class="card-columns">
-            {this.state.booking.map(booking => {
-              return (
-                <>
-                    {/* <table className="table">
-                      <tr>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking Price
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                        <th>
-                          Booking ID
-                        </th>
-                      </tr>
-                    </table> */}
-                </>
-              );
-            })}
-          </div>
-        </div>
+      <div className="container" style={{ backgroundColor: "white", opacity: 1, filter: "Alpha(opacity=100)", borderRadius: '10px' }}>
+
+        <br />
+        <h2>Openhome User Dashboard</h2>
+        <br />
+        <h4>Welcome {this.props.match.params.name}</h4>
+
+
+        <table className="table">
+          <tr>
+            <th>Booking ID</th>
+            <th>Address</th>
+            <th>Description</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Check-IN</th>
+            <th>Check-Out</th>
+            <th>Penalty Reason</th>
+            <th>Penalty Amount</th>
+            <th>Booking Amount</th>
+            <th>Total Payable</th>
+            <th>Status</th>
+            <th></th>
+            <th></th>
+            <th></th>
+
+          </tr>
+          {this.state.booking.map(booking => {
+            return (
+              <tr>
+                <td>{booking.id}</td>
+                <td>{booking.address}</td>
+                <td>{booking.description}</td>
+                <td>{booking.startDate}</td>
+                <td>{booking.endDate}</td>
+                <td>{booking.checkInDate}</td>
+                <td>{booking.checkOutDate}</td>
+                <td>{booking.penaltyReason}</td>
+                <td>{booking.penaltyValue}</td>
+                <td>{booking.bookedPrice}</td>
+                <td>{booking.paymentAmount}</td>
+                <td>{booking.status}</td>
+                <td> <button onClick={() => this.checkIn(booking)} class="btn btn-primary">Check-In</button></td>
+                <td> <button onClick={() => this.checkOut(booking)} class="btn btn-primary">Check-Out</button></td>
+                <td> <button onClick={() => this.cancel(booking)} class="btn btn-primary">Cancel</button></td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
+
+
     );
   }
 }
